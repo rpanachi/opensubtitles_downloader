32a4c550a578ae730b1a98db463ac829657c1bd2
Rodrigo Panachi <rodrigopanachi@gmail.com>
2014-06-01 23:18:58 -0300

    Detectando endianness e convertendo little-endian/big-endian se necessario + binarios

--- oshash.c
+++ oshash.c
@@ -1,31 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void usage(char *name) {
-    printf("Usage: %s <file>\n", name);
-    exit(1);
+  printf("Usage: %s <file>\n", name);
+  exit(1);
+}
+
+int endianness()
+{
+  unsigned int x = 1;
+  return (int) (((char *)&x)[0]);
+}
+
+long long endian_swap(unsigned long long x)
+{
+  x = (x>>56) |
+    ((x<<40) & 0x00FF000000000000) |
+    ((x<<24) & 0x0000FF0000000000) |
+    ((x<<8)  & 0x000000FF00000000) |
+    ((x>>8)  & 0x00000000FF000000) |
+    ((x>>24) & 0x0000000000FF0000) |
+    ((x>>40) & 0x000000000000FF00) |
+    (x<<56);
+  return x;
 }
 
 int main(int argc, char *argv[]) {
-    unsigned long long buf[16384], c = 0;
-    FILE *in;
-    int i;
-    if (argc != 2) {
-        usage(argv[0]);
-    }
-    in = fopen(argv[1], "rb");
-    if (in == NULL) {
-        usage(argv[0]);
-    }
-    fread(buf, 8192, 8, in);
-    fseek(in, -65536, SEEK_END);
-    fread(&buf[8192], 8192, 8, in);
-    for (i = 0; i < 16384; i++) {
-        c+= buf[i];
-    }
-    c+= ftell(in);
-    fclose(in);
-    printf("%016llx\n", c);
-    return 0;
-}
+  unsigned long long buf[16384], c = 0;
+  FILE *in;
+  int i;
+  int en = endianness();
 
+  if (argc != 2) {
+    usage(argv[0]);
+  }
+  in = fopen(argv[1], "rb");
+  if (in == NULL) {
+    usage(argv[0]);
+  }
+
+  fread(buf, 8192, 8, in);
+  fseek(in, -65536, SEEK_END);
+  fread(&buf[8192], 8192, 8, in);
+  for (i = 0; i < 16384; i++) {
+    c += en == 0 ? endian_swap(buf[i]) : buf[i];
+  }
+  c+= ftell(in);
+  fclose(in);
+
+  printf("%016llx\n", c);
+  return 0;
+}
